f45d9af136bfbd6279828ed482401007a71a7dd4
shon615 <shon615@gmail.com>
2016-07-05 23:58:38 -0400

    Switched over to executable Jar File

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,21 @@ public class Main {
 	
 	static ArrayList<Stock> bestStocksPerDate = new ArrayList<Stock>();
 	static PrintWriter writer;
+	static DecimalFormat df = new DecimalFormat("#.##");
+	
 	public static void main(String[] args) throws FileNotFoundException, ParseException, IOException{
 		// TODO Auto-generated method stub
-		
-		//String fileLocation = JOptionPane.showInputDialog("Please input the file location: ");
-		writer = new PrintWriter(System.currentTimeMillis() + "InvestmentStrategy.txt", "UTF-8");
-		Scanner in = new Scanner (new File("in.txt"));
+		JOptionPane.showMessageDialog(null, "Your input txt file should be tab delimetered and the Symbol, Name, Dividend Date, Previous Dividend, and Cost Of Stock");
+		String fileLocation = JOptionPane.showInputDialog("Please input the file location: ");
+		String outputName = System.currentTimeMillis() + "InvestmentStrategy.txt";
+		writer = new PrintWriter(outputName, "UTF-8");
+		Scanner in = new Scanner (new File(fileLocation));
 		
 		in.useDelimiter("\\t|\\r|\\n");
 		//Read in the stocks from a tab-delimited file
 		ArrayList<Stock> stocks = new ArrayList<>();
 		while (in.hasNext()) {
-			DateFormat df 	= DateFormat.getDateInstance(DateFormat.SHORT);
+			DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
 			String next = in.next();
 			if (next.equals("")){
 				continue;
@@ -53,12 +56,12 @@ public class Main {
 				bestStocksPerDate.add(stocks.get(i));
 			}
 		}
-		/*
+		
 		//The following code is if you want to invest 10k in each stock
 		double totalDividend = 0;
 		for (Stock s: bestStocksPerDate) {
 			totalDividend += s.profit;
-			s.print();
+			printAStock(s);
 		}
 		
 		
@@ -68,15 +71,15 @@ public class Main {
 		double totalInvestment = 10000*numberStocks;
 		double percentageProfit = totalDividend/totalInvestment*100;
 		
-		System.out.println("Investing 10k in each:");
-		System.out.println("\tNumber of Stocks: " + numberStocks);
-		System.out.println("\tTotal Investment Needed: " + totalInvestment);
-		System.out.println("\tTotal Dividend Earned: " + df.format(totalDividend));
-		System.out.println("\tPercentage Profit: " + df.format(percentageProfit));
-		System.out.println("\n\n\n");
-		*/
+		writer.println("Investing 10k in each:");
+		writer.println("\tNumber of Stocks: " + numberStocks);
+		writer.println("\tTotal Investment Needed: " + totalInvestment);
+		writer.println("\tTotal Dividend Earned: " + df.format(totalDividend));
+		writer.println("\tPercentage Profit: " + df.format(percentageProfit));
+		writer.println("\n\n\n");
 		
-		// *** THE FOLLOWING CODE USES CONSOLE OUTPUT****
+		
+		/* *** THE FOLLOWING CODE USES CONSOLE OUTPUT****
 		//The following strategies are if you want to only invest $10k
 		//but want the highest dividends 
 		double maxProfit = 0;
@@ -90,75 +93,82 @@ public class Main {
 				bestStrategy = i+1;
 			}
 		}
-		DecimalFormat df = new DecimalFormat("#.##");
 		System.out.println("Suggested Investment Strategy: #" + bestStrategy);
 		System.out.println("Maximum Possible Percentage Profit: " + df.format(maxProfit) + "%");
+		*/
 		
-		/*
 		double maxProfit = 0;
 		int bestStrategy = 0;
 		for (int i = 0; i < bestStocksPerDate.size(); i++) {
 			double tempProfit;
-			System.out.println("Strategy #" + (i+1));
+			writer.println("Strategy #" + (i+1));
 			tempProfit = getStrategy(i);
 			if (tempProfit > maxProfit) {
 				maxProfit = tempProfit;
 				bestStrategy = i+1;
 			}
 		}
-		DecimalFormat df = new DecimalFormat("#.##");
 		writer.println("Suggested Investment Strategy: #" + bestStrategy);
 		writer.println("Maximum Possible Percentage Profit: " + df.format(maxProfit) + "%");
-		*/
+		
 		writer.close();
+		JOptionPane.showMessageDialog(null, "Your input has been processed. Your output can be found in " + outputName);
 	}
 	
 	//Enter 0 for start with the first input stock, 1, 2, 3, for subsequent days
 	public static double getStrategy (int startDay) {
+		//These three are counter variables
 		double totalProfit = 0;
 		int counter = startDay;
 		
+		//this variable is the earliest date that we can start investing at any given moment
+		Date date;
 		
+		//Start investing in the first stock
 		Stock temp = bestStocksPerDate.get(counter);
-		Date date = (Date) temp.dividendDate;
 		totalProfit += temp.profit;
 		printAStock(temp);
 		
+		//push the date to the date at which the money is available to invest again
 		date = temp.moneyBankDate;
 		counter++;
 		
 		while (counter < bestStocksPerDate.size()) {
 			temp = bestStocksPerDate.get(counter);
 			
+			//If the buy date of the current Stock is ahead of when we have money again, invest in it
 			if ((temp.buyDate).compareTo(date) > 0) {
 				
 				printAStock(temp);
 				totalProfit += temp.profit;
 				
+				//push the date back to the date at which the money is available again
 				date = temp.moneyBankDate;
 			}
 			counter++;
 		}
-		double percentageProfit = totalProfit/10000 *100;
-		DecimalFormat df = new DecimalFormat("#.##");
 		
+		double percentageProfit = totalProfit/10000 *100;
+		
+		/*
 		System.out.println("Total Investment: $10,000");
 		System.out.println("Total Dividend Earned: " + df.format(totalProfit));
 		System.out.println("Percentage Profit: " + df.format(percentageProfit) + "%");
 		System.out.println("\n");
+		*/
 		
-		/*writer.println("Total Investment: $10,000");
+		writer.println("Total Investment: $10,000");
 		writer.println("Total Dividend Earned: " + df.format(totalProfit));
 		writer.println("Percentage Profit: " + df.format(percentageProfit) + "%");
-		writer.println("\n");*/
+		writer.println("\n");
 		
 		return percentageProfit;
 	}
 	
 	
 	public static void printAStock (Stock s) {
-		s.print();
-		//System.out.println(s.symbol + "\t" + s.name + "\t" + s.dividendDate + "\t" + s.pastIncome + "\t" + s.costPerStock + "\t" + s.unitsPerTenK + "\t" + s.profit);
+		//s.print();
+		writer.println(s.symbol + "\t" + s.name + "\t" + s.dividendDate + "\t" + s.pastIncome + "\t" + s.costPerStock + "\t" + s.unitsPerTenK + "\t" + df.format(s.profit));
 		writer.println("\t\tBuy Date: " + s.buyDate);
 		writer.println("\t\tSell Date: " + s.sellDate);
 		writer.println("\t\tMoney in Da Bank: " + s.moneyBankDate);
